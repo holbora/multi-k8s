22a76145d548fda9362dff4ea259a2440b956999
bogdan <bogdan.holbora@softvision.com>
2020-06-06 20:36:51 -0700

    Tag images and push them to images repository

--- deploy.sh
+++ deploy.sh
@@ -1,1 +1,12 @@
-exit 0
+docker build -t holbora/multi-client:latest -t holbora/multi-client:$SHA -f ./client/Dockerfile ./client
+docker build -t holbora/multi-server:latest -t holbora/multi-server:$SHA -f ./server/Dockerfile ./server
+docker build -t holbora/multi-worker:latest -t holbora/multi-worker:$SHA -f ./worker/Dockerfile ./worker
+
+docker push holbora/multi-client:latest
+docker push holbora/multi-client:$SHA
+
+docker push holbora/multi-server:latest
+docker push holbora/multi-server:$SHA
+
+docker push holbora/multi-worker:latest
+docker push holbora/multi-worker:$SHA
